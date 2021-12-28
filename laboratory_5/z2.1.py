dd71bd4d2e78d35c68ad2effed959c1461a6cf8f
Anastasiia-Bibika <93943067+Anastasiia-Bibika@users.noreply.github.com>
2021-12-28 20:21:31 +0200

    Update z2.1.py

--- laboratory_5/z2.1.py
+++ laboratory_5/z2.1.py
@@ -6,10 +6,13 @@ n - int - кількість елементів
 """
 n = int(input("Введіть кількість елементів : "))
 num_1 = 0
+k = 0
 for i in range(n):
     num = float(input("number #{0} :".format(i)))
     if num > num_1:
         num_1 = num
-        print("Послідовність зростає")
-    else:
-        break
+        k += 1
+if k == n:
+    print("Послідовність зростає")
+else:
+    print("Послідовність не зростає")
