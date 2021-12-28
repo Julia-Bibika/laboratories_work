39ca8bb26ac669148a4e17dc4aa9fe3eed6e9a09
Anastasiia-Bibika <93943067+Anastasiia-Bibika@users.noreply.github.com>
2021-12-28 21:42:09 +0200

    Update z3.1.py

--- laboratory_6/z3.1.py
+++ laboratory_6/z3.1.py
@@ -1,4 +1,3 @@
-import math
 """
 Ззнайти вектор
 c=2(a +c)- b, де a,b,c є R^n
@@ -8,17 +7,11 @@ c=2(a +c)- b, де a,b,c є R^n
 num - номер елемента вектору а 
 num_1 - номер елемента b
 num_2 - номер елемента c
-sum - сума всіх елментів вектору а
-sum_1 - сума всіх елментів вектору b
-sum_2 - сума всіх елментів вектору c
 """
 n = int(input("Кількість елементів векторів: "))
 a = []
 b = []
 c = []
-sum = 0
-sum_1 = 0
-sum_2 = 0
 for i in range(n):
     num = int(input('Вектору a number #{0} = '.format(i)))
     a.append(num)
@@ -28,16 +21,9 @@ for i in range(n):
 for i in range(n):
     num_2 = int(input('Вектору c number #{0} = '.format(i)))
     c.append(num_2)
-for i in range(n):
-    sum += (a[i]**2)
-for i in range(n):
-    sum_1 += (b[i]**2)
-
-a_length = math.sqrt(sum)
-b_length = math.sqrt(sum_1)
 i = 0
 for i in range(len(c)):
-    num_3 = (2 * (a_length + c[i])) - b_length
+    num_3 = (2 * (a[i] + c[i])) - b[i]
     c.insert(i, num_3)
-    c.pop(i-1)
+    c.pop(i-2)
 print("Вектор c = {0}".format(c))
